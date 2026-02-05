6af1519e5029f0fbefeb5b8579da78efaf2a9855
Iurii Cherkasov <cherkasoooov@gmail.com>
2026-02-05 13:48:49 +0100

    Use consistent single quotes in `seed_senders`.

--- src/altegio_bot/scripts/seed_senders.py
+++ src/altegio_bot/scripts/seed_senders.py
@@ -10,24 +10,24 @@ from altegio_bot.models.models import ServiceSenderRule, WhatsAppSender
 
 SENDERS = [
     {
-        "company_id": 758285,
-        "sender_code": "default",
-        "phone_number_id": "TEST_PHONE_NUMBER_ID_1",
-        "display_phone": "+491742310386",
+        'company_id': 758285,
+        'sender_code': 'default',
+        'phone_number_id': 'TEST_PHONE_NUMBER_ID_1',
+        'display_phone': '+491742310386',
     },
     {
-        "company_id": 758285,
-        "sender_code": "nails",
-        "phone_number_id": "TEST_PHONE_NUMBER_ID_2",
-        "display_phone": "+491700000000",
+        'company_id': 758285,
+        'sender_code': 'nails',
+        'phone_number_id': 'TEST_PHONE_NUMBER_ID_2',
+        'display_phone': '+491700000000',
     },
 ]
 
 RULES = [
     {
-        "company_id": 758285,
-        "service_id": 12859821,
-        "sender_code": "nails",
+        'company_id': 758285,
+        'service_id': 12859821,
+        'sender_code': 'nails',
     },
 ]
 
@@ -37,8 +37,8 @@ async def upsert_sender(data: dict) -> None:
         async with session.begin():
             stmt = (
                 select(WhatsAppSender)
-                .where(WhatsAppSender.company_id == data["company_id"])
-                .where(WhatsAppSender.sender_code == data["sender_code"])
+                .where(WhatsAppSender.company_id == data['company_id'])
+                .where(WhatsAppSender.sender_code == data['sender_code'])
             )
             res = await session.execute(stmt)
             obj = res.scalar_one_or_none()
@@ -47,8 +47,8 @@ async def upsert_sender(data: dict) -> None:
                 session.add(WhatsAppSender(**data))
                 return
 
-            obj.phone_number_id = data["phone_number_id"]
-            obj.display_phone = data.get("display_phone")
+            obj.phone_number_id = data['phone_number_id']
+            obj.display_phone = data.get('display_phone')
             obj.is_active = True
 
 
@@ -57,8 +57,8 @@ async def upsert_rule(data: dict) -> None:
         async with session.begin():
             stmt = (
                 select(ServiceSenderRule)
-                .where(ServiceSenderRule.company_id == data["company_id"])
-                .where(ServiceSenderRule.service_id == data["service_id"])
+                .where(ServiceSenderRule.company_id == data['company_id'])
+                .where(ServiceSenderRule.service_id == data['service_id'])
             )
             res = await session.execute(stmt)
             obj = res.scalar_one_or_none()
@@ -67,7 +67,7 @@ async def upsert_rule(data: dict) -> None:
                 session.add(ServiceSenderRule(**data))
                 return
 
-            obj.sender_code = data["sender_code"]
+            obj.sender_code = data['sender_code']
 
 
 async def main() -> None:
@@ -78,5 +78,5 @@ async def main() -> None:
         await upsert_rule(r)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     asyncio.run(main())
